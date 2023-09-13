c116abde3b2ad073c621360e4142677248a7fa03
ViktorSolomianiy <viktorsolomianiy@gmail.com>
2023-09-13 10:32:26 +0300

    add cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
